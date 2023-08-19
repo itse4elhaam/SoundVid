802a695e3cf6baf48591885ee9e5f525136c72ff
Elhaam <e4elhaam@gmail.com>
2023-08-20 02:42:53 +0500

    upload assets tabs

--- src/app/upload-assets/page.tsx
+++ src/app/upload-assets/page.tsx
@@ -1,101 +1,16 @@
-"use client";
 
-import React, { useState } from "react";
+import UploadTabs  from "src/components/Upload UI/UploadTabs";
 import Footer from "../../components/footer";
 import NavBar from "src/components/Home Page/header/NavBar";
-import { AutoComplete, AutoCompleteProps } from "src/components/AutoComplete";
-import { Upload, X } from "lucide-react";
-import { Button } from "src/components/ui/button";
-import { ButtonProps } from "../../components/ui/button";
+
 
 export default function UploadForm() {
-	const [fileName, setFileName] = useState<string>("");
-
-	const [selectedItems, setSelectedItems] = useState<string[]>([]);
-
-	function addSelectedItems(value: string) {
-		setSelectedItems((prevState) => [...prevState, value]);
-	}
-	function removeItem(value: string) {
-		setSelectedItems((prevItems) =>
-			prevItems.filter((item) => item !== value)
-		);
-	}
-
-	const AutoCompleteData: AutoCompleteProps = {
-		selectedItemsProp: selectedItems,
-		handleSelection: addSelectedItems,
-		name: "category",
-		options: [
-			{
-				value: "summer",
-			},
-			{
-				value: "women's day",
-			},
-			{
-				value: "resolutions",
-			},
-		],
-	};
-
-	function handleFileChange() {
-		console.log("file");
-	}
 
 	return (
 		<>
 			<NavBar SolidBg={true} />
-			<main className="flex justify-center items-center h-screen bg-gray-100">
-				<form className="max-w-md w-full bg-white p-8 rounded-lg shadow-md">
-					<div className="mb-6">
-						<label className="block font-semibold mb-2">
-							Upload File:
-						</label>
-						<input
-							type="file"
-							className="border w-full py-2 px-3 rounded focus:outline-none focus:ring focus:border-blue-300"
-							onChange={handleFileChange}
-						/>
-					</div>
-					<div className="mb-6">
-						<label className="block font-semibold mb-2">
-							File Name:
-						</label>
-						<input
-							type="text"
-							className="border w-full py-2 px-3 rounded focus:outline-none focus:ring focus:border-blue-300"
-							onChange={(e) => setFileName(e.target.value)}
-							value={fileName}
-						/>
-					</div>
-
-					<AutoComplete {...AutoCompleteData} />
-
-					<div className="flex flex-wrap">
-						{selectedItems &&
-							selectedItems.map((item, index) => (
-								<span
-									key={index}
-									className="badge bg-slate-400 text-white py-1 px-2 rounded-xl my-4 space-x-6 justify-center flex flex-wrap"
-								>
-									<span className="text-xs">{item}</span>{" "}
-									<Button
-										variant={"outline"}
-										type="reset"
-										className="p-0 w-fit h-fit bg-transparent"
-										onClick={() => removeItem(item)}
-									>
-										<X className="h-4 w-4" />
-									</Button>
-								</span>
-							))}
-					</div>
-
-					<Button className="my-4" type="submit">
-						<Upload className="mr-2 h-4 w-4" /> Upload
-					</Button>
-				</form>
+			<main className="min-h-[100vh] grid place-content-center my-16">
+				<UploadTabs />
 			</main>
 			<Footer />
 		</>
---
+++ src/components/Upload UI/UploadTabs.tsx
@@ -0,0 +1,46 @@
+import { Button } from "src/components/ui/button";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "src/components/ui/card";
+import { Input } from "src/components/ui/input";
+import { Label } from "src/components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "src/components/ui/tabs";
+import UploadAssets from './UploadAssets';
+
+export default function UploadTabs() {
+	return (
+		<Tabs defaultValue="video" className=" min-w-[400px]">
+			<TabsList className="grid w-full grid-cols-2">
+				<TabsTrigger value="video">Upload Video</TabsTrigger>
+				<TabsTrigger value="audio">Upload Audio</TabsTrigger>
+			</TabsList>
+			<TabsContent value="video">
+				<Card>
+					<CardHeader>
+						<CardTitle>Video Upload</CardTitle>
+						<CardDescription>Upload Videos Here.</CardDescription>
+					</CardHeader>
+					<CardContent className="space-y-2">
+						<UploadAssets />
+					</CardContent>
+				</Card>
+			</TabsContent>
+			<TabsContent value="audio">
+				<Card>
+					<CardHeader>
+						<CardTitle>Audio Upload</CardTitle>
+						<CardDescription>Upload Audio Here.</CardDescription>
+					</CardHeader>
+					<CardContent className="space-y-2">
+						<UploadAssets />
+					</CardContent>
+				</Card>
+			</TabsContent>
+		</Tabs>
+	);
+}
