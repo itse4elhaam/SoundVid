e56d19bf7519993974f4cb8dae8b1f3d81dd71a9
NomanBinBasheer <nomanbinbasheer66626@gmail.com>
2023-08-19 23:04:10 +0500

    added a create new blog page and fixed some responsiveness issues in Header

--- src/components/Home Page/header/Header.tsx
+++ src/components/Home Page/header/Header.tsx
@@ -1,13 +1,18 @@
-import NavBar from './NavBar';
-import HeaderContent from './HeaderContent';
-export default function Header (){
-    return (
-		<header className="relative h-[60vh] bg-black">
-			<video autoPlay loop className="video-home absolute inset-0 h-full w-full object-cover opacity-30 bg-black bg-opacity-25" muted>
-                <source src="https://resources.videvo.net/wp-content/themes/ViDEVO/home-videos/11.mp4" />
-            </video>
-			<NavBar ShowSearchBar={false} />
-            <HeaderContent />
-		</header>
-	);
+import NavBar from "./NavBar";
+import HeaderContent from "./HeaderContent";
+export default function Header() {
+  return (
+    <header className="relative h-[100vh] md:h-[65vh] bg-black">
+      <video
+        autoPlay
+        loop
+        className="video-home absolute inset-0 h-full w-full object-cover opacity-30 bg-black bg-opacity-25"
+        muted
+      >
+        <source src="https://resources.videvo.net/wp-content/themes/ViDEVO/home-videos/11.mp4" />
+      </video>
+      <NavBar ShowSearchBar={false} />
+      <HeaderContent />
+    </header>
+  );
 }
